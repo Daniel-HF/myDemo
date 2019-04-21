8a1d0ebc011bedd4796cec4d614e2dcd6ba04a95
Daniel-HF <18818590229@163.com>
2019-04-21 12:38:30 +0800

    Desc:优化zk 代码 Author:daniel

--- zookeeper-demo/src/main/java/com/daniel/zookeeper/SharedLock/SharedLock.java
+++ zookeeper-demo/src/main/java/com/daniel/zookeeper/SharedLock/SharedLock.java
@@ -1,6 +1,7 @@
-package com.daniel.zookeeper;
+package com.daniel.zookeeper.SharedLock;
 
 import org.apache.zookeeper.CreateMode;
+import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
 import org.apache.zookeeper.ZooDefs;
@@ -11,65 +12,48 @@ import org.apache.zookeeper.data.Stat;
 import java.util.List;
 
 /**
- * zk 服务注册
+ * 用zk 实现分布式共享锁
  *
- * @Author: Daniel.huang
- * @Date: 2019/4/18 10:25
+ * 1.链接zk
+ * 2.注册当前机器
+ * 3.获取锁：每个都注册为带编号的的临时的锁
+ * 4.执行业务逻辑
+ * 5.释放锁
+ * 6.重新注册当前机器
+ *
  */
-public class ZkServerRegister {
 
-    //    private static final String zkUrl = "mini2:2181,mini3:2181,mini4:2181";
+public class SharedLock {
+
     private static final String zkUrl = "centos02:2181,centos03:2181,centos03:2181";
     private static final int timeOut = 1000;
-    private static final String parentPath = "/zk";
-    private static final String thisNode = "/sub";
+    private static final String parentPath = "/lock";
+    private static final String sub="/sub";
+    private static String lockNumber=""; // 当前锁编号
     private ZooKeeper zkCli = null;
 
-    /**
-     * 业务系统注册服务到zk
-     *
-     * @param args
-     */
-    public static void main (String[] args) throws Exception {
-        //获取zk 连接
-        ZkServerRegister register = new ZkServerRegister ();
-        register.getConnect ();
-        //注册zk服务
-        register.serverRegister (args[0]);
-        //业务系统执行操作
-        register.businessLogic (args[0]);
-    }
 
     /**
      * 获取zk connect
      */
-    public void getConnect () throws Exception {
+    public void getConnect() throws Exception {
         zkCli = new ZooKeeper (zkUrl, timeOut, new Watcher () {
             /**
              * 监听器只有一次生命周期，如果想持续监听，需在获得监听后再次监听
              * @param watchedEvent
              */
             @Override
-            public void process (WatchedEvent watchedEvent) {
-                System.out.println ("zk node 监听事件。。。。");
-                //再次监听
+            public void process(WatchedEvent watchedEvent) {
+                // 需要判断下事件类型
+                System.out.println("zk node 监听事件。。。。");
                 try {
-                    zkCli.getChildren ("/", true);
+                    getLock ();
                 } catch (Exception e) {
                     e.printStackTrace ();
                 }
             }
         });
     }
-
-    /**
-     * 创建zk 节点
-     *
-     * 参数1：
-     * 参数2：
-     * 参数3：
-     * 参数4：
-     */
 
     /**
      *
@@ -113,18 +97,60 @@ public class ZkServerRegister {
             String parentNode = createNode (parentPath, hostName,ZooDefs.Ids.OPEN_ACL_UNSAFE,CreateMode.PERSISTENT);
             System.out.println ("父节点创建成功...." + parentNode);
         }
-        String childNode = createNode (parentPath + thisNode, hostName,ZooDefs.Ids.OPEN_ACL_UNSAFE,CreateMode.EPHEMERAL_SEQUENTIAL);
+        String childNode = createNode (parentPath + sub, hostName,ZooDefs.Ids.OPEN_ACL_UNSAFE,CreateMode.EPHEMERAL_SEQUENTIAL);
         System.out.println ("子节点创建成功...."+childNode);
 
     }
 
+
+    public void getLock() throws Exception {
+
+        List<String> children = zkCli.getChildren (parentPath, true);
+        if(children.size () ==1 && lockNumber.equals (children.get (0))) {
+            //只有我一个人，我直接去执行业务操作了
+            execcutBusiness ();
+        } else if(lockNumber.equals (children.get (0))) {
+            //如果有多个，但第一个还是我，我也去执行业务操作了
+            execcutBusiness ();
+        }
+        //再次监听
+        try {
+            zkCli.getChildren(parentPath, true);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
     /**
-     * 业务逻辑
+     * 执行业务操作
+     *
+     * @throws InterruptedException
      */
-    public void businessLogic (String hostName) throws InterruptedException {
-        //执行业务逻辑
-        System.out.println (hostName + " 开始执行业务逻辑了。。。。");
+    public void execcutBusiness() throws Exception {
+        System.out.println ("这事只能我一个人干。。");
+        Thread.sleep (3000);
+        deleteNode ();
+    }
+
+    /**
+     * 删除当前节点
+     *
+     * @throws KeeperException
+     * @throws InterruptedException
+     */
+    public void deleteNode() throws KeeperException, InterruptedException {
+        this.zkCli.delete (parentPath+ lockNumber,-1);
+    }
+
+
+    public static void main (String[] args) throws Exception {
+        SharedLock lock = new SharedLock ();
+        lock.getConnect ();
+        lock.serverRegister (args[0]);
         Thread.sleep (Long.MAX_VALUE);
     }
+
 
 }
